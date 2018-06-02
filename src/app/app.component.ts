05c3023addb4f751487c0b1a28fccf034cf4bebb
giogiojs <gzci24@gmail.com>
2018-06-01 22:15:40 -0500

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  constructor(private _http: HttpClient) {
+    this._http.get('https://api.github.com/users').subscribe(result=>{
+      console.table(result);
+    })
+  }
 }
